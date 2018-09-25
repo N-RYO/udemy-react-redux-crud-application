d02953be9eb2ce41a32835c3d63d80ab6b00f426
apple <apple@apple-no-MacBook-Air.local>
2018-09-25 14:53:31 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import React, {
   Component
 } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//class App extends Component {
+//  render() {
+//    return <h1>Hello, World!</h1>;
+//  }
+//}
+
+//class App extends Component {
+//  render() {
+//    const dom = <h1>Hi!</h1>
+//    return dom;
+//  }
+//}
+
+//class App extends Component {
+//  render() {
+//    const greeting = "Ethereum";
+//    const dom = <h1 className="foo">{greeting}</h1>;
+//    return dom;
+//  }
+//}
 
 class App extends Component {
   render() {
     return ( <
-      div className = "App" >
+      React.Fragment >
       <
-      header className = "App-header" >
-      <
-      img src = {
-        logo
+      label htmlFor = "bar" > bar < /label> <
+      input type = "text"
+      onChange = {
+        () => {
+          console.log("I am clicked!")
+        }
       }
-      className = "App-logo"
-      alt = "logo" / >
-      <
-      h1 className = "App-title" > Hello, World! < /h1> <
-      /header> <
-      p className = "App-intro" >
-      To get started, edit < code > src / App.js < /code> and save to reload. <
-      /p> <
-      /div>
-    );
+      /> <
+      /React.Fragment>  
+    )
   }
 }
 
