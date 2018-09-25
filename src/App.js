eb2807d3e26724fa2b4eef7b98a503a140382322
apple <apple@apple-no-MacBook-Air.local>
2018-09-25 15:38:58 +0900

    create class component and fun functional component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, {
-  Component
-} from 'react';
+//import React, { Component } from 'react';
+import React from 'react';
 
 //class App extends Component {
 //  render() {
@@ -23,22 +22,37 @@ import React, {
 //  }
 //}
 
-class App extends Component {
-  render() {
-    return ( <
-      React.Fragment >
-      <
-      label htmlFor = "bar" > bar < /label> <
-      input type = "text"
-      onChange = {
-        () => {
-          console.log("I am clicked!")
-        }
-      }
-      /> <
-      /React.Fragment>  
-    )
-  }
+//class App extends Component {
+//  render() {
+//    return (
+//      <React.Fragment>
+//        <label htmlFor="bar">bar</label>
+//        <input type="text" onChange={() => {console.log("I am clicked!")}} />
+//      </React.Fragment>  
+//    )
+//  }
+//}
+
+const App = () => {
+  return ( <
+    div >
+    <
+    Cat / >
+    <
+    Cat / >
+    <
+    Cat / >
+    <
+    Cat / >
+    <
+    /div>
+  )
 }
+
+//関数コンポーネント
+const Cat = () => {
+  return <div > Animal! < /div>
+}
+
 
 export default App;
