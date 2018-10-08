5f1d9ebef1c4360117fbe6b56da565e714827215
R-N84 <30769605+N-RYO@users.noreply.github.com>
2018-10-08 22:43:59 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,45 +37,24 @@ import PropTypes from 'prop-types';
 
 
 const App = () => {
-  const profiles = [{
-      name: "Taro",
-      age: 10
-    },
-    {
-      name: "Hanako",
-      age: 5
-    },
-    {
-      name: "NoName",
-      age: 3
-    }
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 }, 
+    { name: "NoName", age: 3}
   ]
-  return ( <
-    div > {
-      profiles.map((profile, index) => {
-        return <User name = {
-          profile.name
-        }
-        age = {
-          profile.age
-        }
-        key = {
-          index
-        }
-        />
-      })
-    } <
-    /div>
+  return ( 
+    <div> 
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      } 
+     </div>
   )
 }
 
 const User = (props) => {
-  return <div > Hi, I am {
-    props.name
-  }, and {
-    props.age
-  }
-  yeard old! < /div>
+  return <div>Hi, I am {props.name}, and {props.age} years old! </div>
 }
 
 User.propTypes = {
