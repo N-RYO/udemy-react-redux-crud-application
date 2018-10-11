9cac51a10332f84d32413d33547d6f3460dcadde
R-N84 <30769605+N-RYO@users.noreply.github.com>
2018-10-11 22:01:38 +0900

    Add files via upload

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import {
+    combineReducers
+} from 'redux'
+import events from './events'
+
+export default combineReducers({
+    events
+})
+// export default combineReducers({ foo, bar, baz })
