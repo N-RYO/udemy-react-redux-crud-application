9cf95cf561be1388f7925ca855b1f16e422bb55c
R-N84 <30769605+R84IEHMTS00x@users.noreply.github.com>
2018-10-31 01:50:48 +0900

    Delete config_20181015210725.js

--- .history/firebase/config_20181015210725.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBCNrQj8EkTqyloRzbFO1r4FkImsfr6wUw",
-    authDomain: "count-app-4ea26.firebaseapp.com",
-    databaseURL: "https://count-app-4ea26.firebaseio.com",
-    projectId: "count-app-4ea26",
-    storageBucket: "count-app-4ea26.appspot.com",
-    messagingSenderId: "243729339385"
-};
